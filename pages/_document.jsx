73c914d5df82fd4d6ec72f163e32d39fb5dc1463
Xama30 <xavier.breton30@gmail.com>
2026-04-13 16:33:05 -0400

    fix lang tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,8 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+function resolveDocumentLang(nextData) {
+  const locale = String(nextData?.props?.pageProps?.locale || "").toLowerCase();
+  const page = String(nextData?.page || "");
+  const isEnglishLocale = locale.startsWith("en");
+  const isEnglishPage = page === "/en" || page.startsWith("/en/");
+
+  return isEnglishLocale || isEnglishPage ? "en-CA" : "fr-CA";
+}
+
+export default function Document(props) {
+  const lang = resolveDocumentLang(props?.__NEXT_DATA__);
+
   return (
-    <Html lang="fr-CA">
+    <Html lang={lang}>
       <Head>
         <meta charSet="utf-8" key="charset" />
         <link
